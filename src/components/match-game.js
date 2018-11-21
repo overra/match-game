a00261b90542cb1aab48f7f0092c730f7335a9e5
Adam Snodgrass <overra@gmail.com>
2018-11-21 00:46:06 -0600

    import css from parcel instead of css in js

--- src/components/match-game.js
+++ src/components/match-game.js
@@ -1,30 +1,22 @@
 import { html } from "htm/preact/standalone";
-import styled from "./styled";
 import Tilt from "./tilt";
-
-const Tiles = styled("div")({
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr 1fr",
-  gridTemplateRows: "1fr 1fr 1fr 1fr",
-  gridGap: "10px",
-  "> div": {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#ccc"
-  }
-});
+import "./match-game.css";
 
 export default () => {
   const tiles = Array(16).fill(0);
   return html`
     <section>
       <h1>Match Game</h1>
-      <${Tiles}>${tiles.map(
-    tile =>
-      html`
+      <div class="tiles">
+        ${
+          tiles.map(
+            tile =>
+              html`
         <${Tilt}>tile<//>
       `
-  )}<//>
+          )
+        }
+      </div>
     </section>
   `;
 };
